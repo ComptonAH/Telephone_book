46801701ecf58d9028afc8ff6801d7c605d61ac3
17th <17th@inbox.ru>
2023-05-30 11:46:28 +0300

    Классы

--- controller.py
+++ controller.py
@@ -4,30 +4,32 @@ import model
 
 
 def start():
+    my_pb = model.PhoneBook()
     while True:
         choice = view.main_menu()
 
         match choice:
             case 1:
-                model.open_pb()
+                my_pb.open()
                 view.print_message(text.load_successful)
             case 2:
-                model.save_pb()
+                my_pb.save()
                 view.print_message(text.save_successful)
             case 3:
-                pb = model.get_pb()
+                pb = my_pb.load()
                 view.print_contacts(pb, text.pb_empty)
+                # my_pb.show()
             case 4:
                 contact = view.input_contact(text.input_new_contact)
-                name = model.add_contact(contact)
+                name = my_pb.add(contact)
                 view.print_message(text.new_contact_successful(name))
             case 5:
                 key_word = view.input_search(text.input_search)
-                result = model.search_contact(key_word)
+                result = my_pb.search(key_word)
                 view.print_contacts(result, text.empty_search(key_word))
             case 6:
                 key_word = view.input_search(text.input_change)
-                result = model.search_contact(key_word)
+                result = my_pb.search(key_word)
                 if result:
                     if len(result) != 1:
                         view.print_contacts(result, '')
@@ -35,7 +37,7 @@ def start():
                     else:
                         current_id = result[0].get('id')
                     new_contact = view.input_contact(text.change_contact)
-                    name = model.change_contact(new_contact, current_id)
+                    name = my_pb.change(new_contact, current_id)
                     view.print_message(text.change_successful(name))
                 else:
                     view.print_message(text.empty_search(key_word))
